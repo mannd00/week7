53eba0d2999f4aa064589e09f07df4000ed5be07
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-02-27 18:55:01 -0600

    Update Lab7_Ch6_Mann.sh

--- Lab7_Ch6_Mann.sh
+++ Lab7_Ch6_Mann.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
 
+echo "Welcome to Week 7 Script for Chapter 6"
+echo "We will be creating a unit file for a service and see what it can do."
 
+
+
+
+echo "We have this test service called Lab7.service. Lets install it!"
+cp Lab7.service /etc/systemd/system/
+
+echo "And run it!"
+systemctl start Lab7.service
+
+echo "Now we can check it's status"
+systemctl status Lab7.service
+
+echo "Clean up, deleting our test files."
+
+systemctl stop Lab7.service
+rm /etc/systemd/system/Lab7.service
