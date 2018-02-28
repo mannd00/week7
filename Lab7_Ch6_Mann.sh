8c461d496c001ad8a62d48d230d49e92f21b165a
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-02-27 21:10:03 -0600

    Update Lab7_Ch6_Mann.sh

--- Lab7_Ch6_Mann.sh
+++ Lab7_Ch6_Mann.sh
@@ -3,8 +3,8 @@
 echo "Welcome to Week 7 Script for Chapter 6"
 echo "We will be creating a unit file for a service and see what it can do."
 
-
-
+echo "We can check the systemd configuration path with systemctl -p UnitPath show"
+systemctl -p UnitPath show
 
 echo "We have this test service called Lab7.service. Lets install it!"
 cp Lab7.service /etc/systemd/system/
@@ -15,7 +15,11 @@ systemctl start Lab7.service
 echo "Now we can check it's status"
 systemctl status Lab7.service
 
+echo "And check all current jobs"
+systemctl list-jobs
+
 echo "Clean up, deleting our test files."
 
 systemctl stop Lab7.service
+systemctl disable Lab7.service
 rm /etc/systemd/system/Lab7.service
